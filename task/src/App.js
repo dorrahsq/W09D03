39816a00c28dfd8890144327696404c3f8ad6231
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-12-07 19:04:54 +0300

    routes

--- task/src/App.js
+++ task/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/logIn/login";
+import Header from "./components/header/header";
+import SignUp from "./components/signup/signup";
+import Home from "./components/home/home";
+import Users from "./components/users/users";
+import OneUser from "./components/oneuser/oneuser";
 
 function App() {
   return (
     <>
-app
+      <Header />
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/usres" element={<Users />} />
+        <Route exact path="/user/:id" element={<OneUser />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/" element={<Login />} />
+      </Routes>
     </>
   );
 }
