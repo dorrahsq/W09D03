cf3353f4425e3ae0108ec4a2304162b4050b7ca2
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-12-07 19:34:42 +0300

    signup

--- task/src/components/signup/signup.js
+++ task/src/components/signup/signup.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { logIn } from "../../reducers/login";
+import { useDispatch } from "react-redux";
 
 const Signup = () => {
-  return <>sign up </>;
+  let navigate = useNavigate();
+  const dispatchEvent = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const getUser = async () => {
+    const users = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/user/create`,
+      { email, password, role: "61a4e07ba6502019b9898c1c" }
+    );
+    if (users.status == 204) {
+      console.log(users);
+      setMessage(
+        "this email already hava an account! log in or change your email"
+      );
+    } else {
+      const data = {
+        role: users.data.result.role,
+        token: users.data.token,
+        userID: users.data.result._id,
+      };
+      dispatchEvent(logIn(data));
+      navigate(`/home`);
+    }
+  };
+
+  return (
+    <>
+      <div className="describeItem">
+        <span className="Logg">sign up </span>
+        <input
+          type="text"
+          placeholder=" email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder=" password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className="LogBtn"
+          onClick={() => {
+            getUser();
+          }}
+        >
+          <BsFillArrowRightCircleFill className="goIcon" />
+        </button>
+        <div className="already">
+          already have an account? <Link to="/">log in </Link>
+        </div>
+        <div className="mesageL">{message} </div>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
